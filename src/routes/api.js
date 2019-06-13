dcfbacf02bfa4031d1bf2ff4c323330a9812574b
BerkoBob <alever@omniis.com>
2019-06-13 20:06:05 +0100

    Look at controllers

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,15 +1,22 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const Trade = require("../models/trade");
-const validate = require("../utils/validate");
+const Trade = require('../models/trade');
+const validate = require('../utils/validate');
 
-router.get("/", (req, res, next) => {
-    Trade.find({})
+router.get('/', (req, res, next) => {
+    let query = {};
+    console.log(req.headers.accept);
+    // console.log('Content Type:', req.headers.content - type);
+
+    if (req.query.id) query._id = req.query.id;
+    if (req.query.symbol) query.symbol = req.query.symbol;
+
+    Trade.find(query)
         .then(result => res.status(200).send(result))
         .catch(err => res.status(500).send(err));
 });
 
-router.post("/", (req, res) => {
+router.post('/', (req, res) => {
     const trade = new Trade(validate(req.body));
     trade
         .save()
@@ -17,7 +24,7 @@ router.post("/", (req, res) => {
         .catch(error => res.status(400).send(error));
 });
 
-router.get("/:tradeID", (req, res) => {
+router.get('/:tradeID', (req, res) => {
     Trade.findById(req.params.tradeID)
         .then(data => {
             if (data) res.status(200).send([data]);
@@ -26,7 +33,7 @@ router.get("/:tradeID", (req, res) => {
         .catch(err => res.status(500).send(err));
 });
 
-router.delete("/:tradeID", (req, res) => {
+router.delete('/:tradeID', (req, res) => {
     Trade.remove({ _id: req.params.tradeID })
         .then(result => {
             console.log(result);
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,22 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const request = require('request');
 
-router.get("/", (req, res) => {
-    res.render("home", {
-        name: "Antoine",
+router.get('/', (req, res) => {
+    res.render('home', {
+        name: 'Antoine',
         age: 38,
     });
+});
+
+router.get('/test', (req, res) => {
+    request(
+        'http://localhost:3000/api?id=5d0287456137ab41a424b957',
+        (err, data) => {
+            if (err) throw err;
+            res.send(data.body);
+        }
+    );
 });
 
 module.exports = router;
