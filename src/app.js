9a34637cfcf544b02fffff97a9457db91d62a28a
berkobob <alever@omniis.com>
2019-07-03 15:09:25 +0100

    Change open

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const exphbs = require('express-handlebars');
-const moment = require('moment');
-const numeral = require('numeral');
-const favicon = require('serve-favicon');
-const fileupload = require('express-fileupload');
-const bodyParser = require('body-parser');
-const Handlebars = require('handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const moment = require("moment");
+const numeral = require("numeral");
+const favicon = require("serve-favicon");
+const fileupload = require("express-fileupload");
+const bodyParser = require("body-parser");
+const Handlebars = require("handlebars");
 
-const apiRoutes = require('./routes/api');
-const webRoutes = require('./routes/web');
+const apiRoutes = require("./routes/api");
+const webRoutes = require("./routes/web");
 
 const app = express();
 
@@ -25,57 +25,66 @@ mongoose
     .then(() => console.log(`Connected to ${process.env.MONGODB}`))
     .catch(err =>
         console.log(
-            'Cannot find',
+            "Cannot find",
             process.env.MONGODB,
-            '\nDatabase connection error:',
+            "\nDatabase connection error:",
             err.errmsg,
-            '\n',
-            err
-        )
+            "\n",
+            err,
+        ),
     );
 
 // Use handlebars at templating engine
-numeral.defaultFormat('$0,0.00');
+numeral.defaultFormat("$0,0.00");
 app.engine(
-    'hbs',
+    "hbs",
     exphbs({
-        defaultLayout: 'main',
-        extname: 'hbs',
+        defaultLayout: "main",
+        extname: "hbs",
         helpers: {
-            formatDate: date => (date ? moment(date).format('DD/MM/YY') : ''),
-            formatStrike: amt => (amt ? numeral(amt).format() : ''),
-            formatCash: amt => numeral(amt).format(''),
-            formatNums: qty => numeral(qty).format('0,0'),
-            formatJson: json => JSON.stringify(json).replace(/ /g, '_'),
+            formatDate: date => (date ? moment(date).format("DD/MM/YY") : ""),
+            formatStrike: amt => (amt ? numeral(amt).format() : ""),
+            formatCash: amt => numeral(amt).format(""),
+            formatNums: qty => numeral(qty).format("0,0"),
+            formatJson: json => JSON.stringify(json).replace(/ /g, "_"),
             test: (error, trade) =>
                 trade.errors.includes(error)
                     ? new Handlebars.SafeString(
                           `<strong class="w3-red">${
-                              trade['trade'][error]
-                          }</strong>`
+                              trade["trade"][error]
+                          }</strong>`,
                       )
-                    : trade['trade'][error],
+                    : trade["trade"][error],
             closedExist: trades => trades.length,
+            daysSince: date => moment().diff(moment(date), "days"),
+            putOrCall: trades => trades[0].putOrCall,
+            expiry: trades =>
+                trades[0].expiry
+                    ? moment(trades[0].expiry).format("DD/MM/YY")
+                    : "",
+            strike: trades =>
+                trades[0].strike ? numeral(trades[0].strike).format() : "",
+            multiplier: trades => trades[0].multiplier,
         },
-    })
+    }),
 );
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
 
 // Config express
-app.use(favicon(path.join(__dirname, '../public/img', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(morgan('dev')); // Logger
+app.use(favicon(path.join(__dirname, "../public/img", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(morgan("dev")); // Logger
 app.use(fileupload());
-app.use(bodyParser.urlencoded({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ limit: "1mb" }));
 app.use(bodyParser.json());
 
-app.use('/api', apiRoutes); // API router
+app.use("/api", apiRoutes); // API router
 app.use(webRoutes);
 
 // This last route is a catchall to capture not found requests
 app.use((req, res, next) => {
-    const error = new Error('Not found');
+    const error = new Error("Not found");
     error.status = 404;
     next(error);
 });
